2e22b1b02c6cae6c7a7dd67efed8ad6ef3c24de5
yexi <yexijiang@gmail.com>
2013-08-12 23:00:08 -0400

    reorganize bi-prediction

---
+++ bi-prediction/src/main/java/kdrg/bi/prediction/App.java
@@ -0,0 +1,13 @@
+package kdrg.bi.prediction;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
